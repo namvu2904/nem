3d8c186fb2a0ed6233c7717cfb40f582ac6c2ea5
Nem <namvu2839@gmail.com>
2023-02-22 14:38:41 +0700

    update

--- src/components/Tableuser.js
+++ src/components/Tableuser.js
@@ -6,36 +6,7 @@ import { useGetUsersQuery } from '@/redux/Slicer/slice.user';
 export default function TableUser() {
     const { Column, ColumnGroup } = Table;
     const { data,isLoading} = useGetUsersQuery()
-//     const data = [
-//   {
-//     id:1,
-//     Name:'Nam',
-//     Gender:'Male',
-//     Birth:'29/4/2002',
-//     Experience:'4',
-//   },
-//   {
-//     id:2,
-//     Name:'Thinh',
-//     Gender:'Male',
-//     Birth:'29/1/2002',
-//     Experience:'3',
-//   },
-//   {
-//     id:3,
-//     Name:'Nu',
-//     Gender:'Female',
-//     Birth:'14/5/1999',
-//     Experience:'5',
-//   },
-//     {
-//     id:4,
-//     Name:'Seng',
-//     Gender:'Male',
-//     Birth:'14/5/1999',
-//     Experience:'55',
-//   },
-// ]
+
   const columns =[
     {
       title:"Name",
